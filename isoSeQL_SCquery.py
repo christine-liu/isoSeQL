4dcc1548e6f9113f0d8d5aa6e67d55e4ffc26ab2
christine.liu <christine.liu@berkeley.edu>
2022-05-09 18:20:57 -0700

    update queries_start of single cell

--- isoSeQL_SCquery.py
+++ isoSeQL_SCquery.py
@@ -9,6 +9,10 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import datetime
 import argparse
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.full_figure_for_development(fig,warn=False)
 
 def isoprop_plot(db, exp, outPrefix):
 	conn=sqlite3.connect(db)
@@ -16,6 +20,59 @@ def isoprop_plot(db, exp, outPrefix):
 	exp_file=open(exp, "r")
 	exp_list=exp_file.readlines()
 	exp_list=[i.rstrip() for i in exp_list]
+	#by read
+	df_prop=pd.read_sql("SELECT i.category,SUM(c.read_count),s.exp,s.celltype FROM scCounts c LEFT OUTER JOIN isoform i on i.id=c.isoform_id INNER JOIN scInfo s on s.id=c.scID WHERE c.scID IN (SELECT id FROM scInfo WHERE exp IN (%s)) GROUP BY i.category,s.exp,s.celltype" % ','.join('?' for i in exp_list), conn, params=exp_list)
+	calc_totals=pd.read_sql("SELECT s.exp, SUM(c.read_count) FROM scCounts c INNER JOIN scInfo s on s.id=c.scID WHERE c.scID IN (SELECT id FROM scInfo WHERE exp IN (%s)) GROUP BY s.exp" % ','.join('?' for i in exp_list), conn, params=exp_list)
+	calc_totals.rename(columns={'SUM(c.read_count)':'Total'}, inplace=True)
+	prop = df_prop.merge(calc_totals, on=["exp"])
+	prop['Proportion']=prop['SUM(c.read_count)']/prop['Total']
+	prop['exp_celltype']=prop['exp'].map(str)+'_'+prop['celltype']
+	cell_totals=pd.read_sql("SELECT SUM(c.read_count),s.exp, s.celltype FROM scCounts c INNER JOIN scInfo s on s.id=c.scID WHERE c.scID IN (SELECT id FROM scInfo WHERE exp IN (%s)) GROUP BY s.exp, s.celltype" % ','.join('?' for i in exp_list), conn, params=exp_list)
+	cell_totals['exp_celltype']=cell_totals['exp'].map(str)+'_'+cell_totals['celltype']
+	cell_totals.rename(columns={'SUM(c.read_count)':'cellTotal'}, inplace=True)
+	cell_totals=cell_totals[["cellTotal","exp_celltype"]]
+	prop=prop.merge(cell_totals,on="exp_celltype")
+	prop['celltypeProportion']=prop['SUM(c.read_count)']/prop['cellTotal']
+	fig = go.Figure()
+	fig.update_layout(
+	    template="simple_white",
+	    xaxis=dict(title_text="Exp"),
+	    yaxis=dict(title_text="Proportion"),
+	    barmode="stack",
+	)
+	colorDict={"full-splice_match":'#1d2f5f', 'incomplete-splice_match':'#8390FA', 'novel_in_catalog':'#6eaf46', 'novel_not_in_catalog':'#FAC748', 'antisense':'#00bcc0', 'intergenic':'#fa8800', 'genic':'ac0000', 'genic_intron':'#0096ff', 'fusion':'cf33ac'}
+	category2plot=prop.category.unique().tolist()
+	colors=[colorDict[i] for i in category2plot]
+	for r, c in zip(category2plot, colors):
+	    plot_df = prop[prop.category == r]
+	    fig.add_trace(
+	        go.Bar(x=[plot_df.exp, plot_df.celltype], y=plot_df.Proportion, name=r, marker_color=c),
+	    )
+	readPlotFile = outPrefix+"_isoCelltypeReadPropPlot.pdf"
+	fig.write_image(readPlotFile)
+	print("Isoform read proportions plot saved: " + readPlotFile)
+	fig = go.Figure()
+	fig.update_layout(
+	    template="simple_white",
+	    xaxis=dict(title_text="Exp"),
+	    yaxis=dict(title_text="Proportion"),
+	    barmode="stack",
+	)
+	for r, c in zip(category2plot, colors):
+	    plot_df = prop[prop.category == r]
+	    fig.add_trace(
+	        go.Bar(x=[plot_df.exp, plot_df.celltype], y=plot_df.celltypeProportion, name=r, marker_color=c),
+	    )
+	readPlotFile=outPrefix+"_isoCelltypeNormReadPropPlot.pdf"
+	fig.write_image(readPlotFile)
+	print("Isoform read proportions plot saved: " + readPlotFile)
+	df_bed['name'] = df_bed['id'].map(str)+'_'+df_bed['start'].map(str)+'_'+df_bed['end'].map(str)
+
+
+
+
+################
+
 	df_prop=pd.read_sql("SELECT i.category,COUNT(i.category),c.exp FROM counts c INNER JOIN isoform i on i.id = c.isoform_id WHERE c.exp IN (%s) GROUP BY i.category,c.exp" % ','.join('?' for i in exp_list), conn, params=exp_list)
 	pivot=df_prop.pivot(index="exp", columns="category", values="COUNT(i.category)")
 	cats = pivot.columns.tolist()
@@ -28,7 +85,7 @@ def isoprop_plot(db, exp, outPrefix):
 	pivot_plot.to_csv(tableFile, sep='\t', index=True, header=True)
 	print("Isoform proportions table saved: " + tableFile)
 	plotFile=outPrefix+"_isoPropPlot.pdf"
-	ax=pivot_plot.plot.bar(stacked=True, color={'full-splice_match':'#1d2f5f', 'incomplete-splice_match':'#8390fa', 'novel_in_catalog':'#6eaf46', 'novel_not_in_catalog':'#fac748', 'antisense':'#00bcc0', 'genic':'#ac0000', 'intergenic':'#fa8800', 'fusion':'#cf33ac', 'genic_intron':'#0096ff'}).legend(bbox_to_anchor=(1,1), fontsize=8)
+	ax=pivot_plot.plot.bar(stacked=True, color={'full-splice_match':'#006e00', 'incomplete-splice_match':'#008cf9', 'novel_in_catalog':'#b80058', 'novel_not_in_catalog':'#ebac23', 'antisense':'#00bbad', 'genic':'#d163e6', 'intergenic':'#b24502', 'fusion':'#ff9287', 'genic_intron':'#5954d6'}).legend(bbox_to_anchor=(1,1), fontsize=8)
 	plt.subplots_adjust(right=0.6)
 	plt.savefig(plotFile)
 	print("Isoform proportions plot saved: " + plotFile)
@@ -43,37 +100,14 @@ def isoprop_plot(db, exp, outPrefix):
 	pivot_filterReads.to_csv(readTableFile, sep='\t', index=True, header=True)
 	print("Isoform read proportions table saved: " + readTableFile)
 	readPlotFile = outPrefix+"_isoReadPropPlot.pdf"
-	ax=pivot_filterReads.plot.bar(stacked=True, color={'full-splice_match':'#1d2f5f', 'incomplete-splice_match':'#8390fa', 'novel_in_catalog':'#6eaf46', 'novel_not_in_catalog':'#fac748', 'antisense':'#00bcc0', 'genic':'#ac0000', 'intergenic':'#fa8800', 'fusion':'#cf33ac', 'genic_intron':'#0096ff'}).legend(bbox_to_anchor=(1,1), fontsize=8)
+	ax=pivot_filterReads.plot.bar(stacked=True, color={'full-splice_match':'#006e00', 'incomplete-splice_match':'#008cf9', 'novel_in_catalog':'#b80058', 'novel_not_in_catalog':'#ebac23', 'antisense':'#00bbad', 'genic':'#d163e6', 'intergenic':'#b24502', 'fusion':'#ff9287', 'genic_intron':'#5954d6'}).legend(bbox_to_anchor=(1,1), fontsize=8)
 	plt.subplots_adjust(right=0.6)
 	plt.savefig(readPlotFile)
 	print("Isoform read proportions plot saved: " + readPlotFile)
 	return
 
 
-def make_bed(db, exp, outPrefix, name):
-	conn=sqlite3.connect(db)
-	c=conn.cursor()
-	exp_file=open(exp, "r")
-	exp_list=exp_file.readlines()
-	exp_list=[i.rstrip() for i in exp_list]
-	name_file=open(name, "r")
-	name_list=name_file.readlines()
-	name_list=[i.rstrip() for i in name_list]
-	e=0
-	while e < len(exp_list):
-		df_bed = pd.read_sql("SELECT DISTINCT i.id,i.chr,x.start,x.end,i.strand,i.category,i.iso_exons,i.junctions, x.ex_sizes,x.ex_starts FROM isoform i INNER JOIN isoform_ends x on i.id=x.isoform_id WHERE x.isoform_id IN (SELECT e.ends_id FROM ends_counts e WHERE e.exp = ?)", conn, params=(exp_list[e],))
-		df_bed['name'] = df_bed['id'].map(str)+'_'+df_bed['start'].map(str)+'_'+df_bed['end'].map(str)
-		df_bed['score'] = 60
-		df_bed['RGB'] = df_bed['category'].apply(lambda x: "29,47,95" if x == "full-splice_match" else '131,144,250' if x=="incomplete-splice_match" else '110,175,70' if x=="novel_in_catalog" else '250,199,72' if x=="novel_not_in_catalog" else '0,188,192' if x=="antisense" else '250,136,0' if x=="intergenic" else '172,0,0' if x=="genic" else '0,150,255' if x=='genic_intron' else '207,51,172' if x=="fusion" else '255,0,0')
-		df_bed = df_bed[['chr','start','end','name','score','strand','start','end','RGB','iso_exons','ex_sizes','ex_starts']]
-		filename=outPrefix+"_"+name_list[e]+".bed"
-		df_bed.to_csv(filename, sep='\t', index=False, header=False)
-		header="track visibility=2 itemRgb=On name='"+name_list[e]+"'\n"
-		with open(filename, "r+") as f: s=f.read(); f.seek(0); f.write(header + s)
-		print("Bedfile saved: " + filename)
-		e+=1
-	return
-
+###
 def gene_FSM(db, exp, outPrefix, genes):
 	conn=sqlite3.connect(db)
 	c=conn.cursor()
@@ -101,29 +135,8 @@ def gene_FSM(db, exp, outPrefix, genes):
 		print("FSM read proportions plot saved: " + fileName)
 	return
 
-def IEJ_table(db, exp, out):
-	conn=sqlite3.connect(db)
-	c=conn.cursor()
-	exp_file=open(exp, "r")
-	exp_list=exp_file.readlines()
-	exp_list=[i.rstrip() for i in exp_list]
-	df_IEJ=pd.read_sql("SELECT i.id, i.gene, c.exp, c.read_count FROM isoform i INNER JOIN counts c on c.isoform_id = i.id WHERE i.IEJ = 'TRUE' AND c.exp IN (%s) GROUP BY i.gene" % ','.join('?' for i in exp_list), conn, params=exp_list)
-	df_IEJ['IEJ_id'] = df_IEJ['gene'] + "_" + df_IEJ['id'].astype(str)
-	df_IEJ_pivot=df_IEJ.pivot(index="IEJ_id", columns="exp", values="read_count")
-	df_IEJ_pivot=df_IEJ_pivot.fillna(0)
-	df_IEJ_pivot.to_csv(out, sep='\t')
-	print("IEJ table saved: " + out)
-	return
 
-def expInfo(db, out):
-	conn=sqlite3.connect(db)
-	c=conn.cursor()
-	info_table = pd.read_sql("SELECT * FROM exp e INNER JOIN sampleData s on e.sample_id=s.id GROUP BY e.id", conn)
-	info_table.columns.values[0]="exp_id"
-	info_table = info_table.drop(['id'], axis=1)
-	info_table.to_csv(out, sep='\t', index=False, header=True)
-	print("Sample info table saved: " + out)
-	return
+
 
 def countMatrix(db, exp, outPrefix, gene=False, variable=False):
 	conn=sqlite3.connect(db)
@@ -165,39 +178,6 @@ def countMatrix(db, exp, outPrefix, gene=False, variable=False):
 			print("Variable ends counts matrix saved: " + filename)
 			return
 
-def tappASgff(db, exp, out):
-	conn=sqlite3.connect(db)
-	c=conn.cursor()
-	exp_file=open(exp, "r")
-	exp_list=exp_file.readlines()
-	exp_list=[i.rstrip() for i in exp_list]
-	gff3TX=pd.read_sql("SELECT DISTINCT i.id, i.chr, i.strand, i.gene, i.junctions, i.category, x.start, x.end, x.ex_sizes, t.tx FROM isoform i LEFT OUTER JOIN txID t on i.id=t.isoform_id INNER JOIN isoform_ends x on i.id=x.isoform_id WHERE x.isoform_id IN (SELECT e.ends_id FROM ends_counts e WHERE e.exp IN (%s)) " % ','.join('?' for i in exp_list), conn, params=exp_list)
-	gff3TX['isoform'] = gff3TX['id'].map(str)+'_'+gff3TX['start'].map(str)+'_'+gff3TX['end'].map(str)
-	gff3TX['tx'].replace(np.nan, "novel", inplace=True)
-	outFile = open(out, "w+")
-	for i, row in gff3TX.iterrows():
-		length=sum(int(x) for x in row['ex_sizes'].split(","))
-		txLine=row['isoform']+"\ttappAS\ttranscript\t1\t"+str(length)+"\t.\t"+row['strand']+"\t.\tID="+row['tx']+"; primary_class="+row['category']+'; PosType=T\n'
-		a=outFile.write(txLine)
-		geneLine=row['isoform']+"\ttappAS\tgene\t1\t"+str(length)+"\t.\t"+row['strand']+"\t.\tID="+row['gene']+"; Name="+row['gene']+"; Desc="+row['gene']+"; PosType=T\n"
-		a=outFile.write(geneLine)
-		cdsLine=row['isoform']+"\ttappAS\tCDS\t.\t.\t.\t"+row['strand']+"\t.\tID=Protein_"+row['isoform']+"; Name=Protein_"+row['isoform']+'; Desc=Protein_'+row['isoform']+'; PosType=T\n'
-		a=outFile.write(cdsLine)
-		genomicLine=row['isoform']+"\ttappAS\tgenomic\t1\t1\t.\t"+row['strand']+"\t.\tChr="+row['chr']+'; PosType=G\n'
-		a=outFile.write(genomicLine)
-		jxn_len=len(row['junctions'])
-		if jxn_len > 2:
-			jxns=row['junctions'][2:-2].split('), (')
-			j=0
-			while j < len(jxns):
-				startJ=jxns[j].split(", ")[0]
-				endJ=jxns[j].split(", ")[1]
-				sjLine=row['isoform']+"\ttappAS\tsplice_junction\t"+str(startJ)+"\t"+str(endJ)+"\t.\t"+row['strand']+"\t.\tID_junction_"+str(i)+"; Chr="+row['chr']+"; PosType=G\n"
-				a=outFile.write(sjLine)
-				j+=1
-	print("tappAS-compatible gff3 generated: " + out)
-	return	
-
 
 def main():
 	parser=argparse.ArgumentParser(description="built-in queries to generate plots/tables/visualization of exp/genes of interest")
@@ -206,49 +186,25 @@ def main():
 	isoProp_parser.add_argument('--db')
 	isoProp_parser.add_argument('--exp')
 	isoProp_parser.add_argument('--outPrefix')
-	bed_parser = subparsers.add_parser('bed')
-	bed_parser.add_argument('--db')
-	bed_parser.add_argument('--exp')
-	bed_parser.add_argument('--outPrefix')
-	bed_parser.add_argument('--name')
 	FSM_parser = subparsers.add_parser('FSM')
 	FSM_parser.add_argument('--db')
 	FSM_parser.add_argument('--exp')
 	FSM_parser.add_argument('--outPrefix')
 	FSM_parser.add_argument('--genes')
-	IEJtab_parser = subparsers.add_parser('IEJtab')
-	IEJtab_parser.add_argument('--db')
-	IEJtab_parser.add_argument('--exp')
-	IEJtab_parser.add_argument('--out')
-	sample_parser = subparsers.add_parser('expInfo')
-	sample_parser.add_argument('--db')
-	sample_parser.add_argument('--out')
 	countMat_parser = subparsers.add_parser('countMatrix')
 	countMat_parser.add_argument('--db')
 	countMat_parser.add_argument('--exp')
 	countMat_parser.add_argument('--outPrefix')
 	countMat_parser.add_argument('--gene', action='store_true')
 	countMat_parser.add_argument('--variable', action='store_true')
-	tappASgff_parser = subparsers.add_parser('tappASgff')
-	tappASgff_parser.add_argument('--db')
-	tappASgff_parser.add_argument('--exp')
-	tappASgff_parser.add_argument('--out')
-
+	
 	args=parser.parse_args()
 	if args.subparser_name == "isoProp":
 		isoprop_plot(args.db, args.exp, args.outPrefix)
-	elif args.subparser_name == "bed":
-		make_bed(args.db, args.exp, args.outPrefix, args.name)
 	elif args.subparser_name == "FSM":
 		gene_FSM(args.db, args.exp, args.outPrefix, args.genes)
-	elif args.subparser_name == "IEJtab":
-		IEJ_table(args.db, args.exp, args.out)
-	elif args.subparser_name=="expInfo":
-		expInfo(args.db, args.out)
 	elif args.subparser_name=="countMatrix":
 		countMatrix(args.db, args.exp, args.outPrefix, args.gene, args.variable)
-	elif args.subparser_name=="tappASgff":
-		tappASgff(args.db, args.exp, args.out)
 	else:
 		print("\n***ERROR***\n"+args.subparser_name+" is not an option.\n**********\n")
 
