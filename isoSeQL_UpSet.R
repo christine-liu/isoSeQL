dabe4d344efb4195e72ece01d1531bb83ef6e83b
christine.liu <christine.liu@berkeley.edu>
2022-10-05 11:42:04 -0700

    upset R plotting fxn

--- isoSeQL_UpSet.R
+++ isoSeQL_UpSet.R
@@ -6,7 +6,7 @@ library(UpSetR)
 option_list=list(
 	make_option(c('-i', '--input'), help='input binary matrix with experiments as columns'),
 	make_option(c('-o', '--outfile'), help='path to output file'),
-	make_option(c('-n', '--numSamples'), help='number of groups being compared')
+	make_option(c('-n', '--numSamples'), help='number of groups being compared'),
 	make_option(c('-t', '--top'), help='show top x overlaps')
 )
 opt= parse_args(OptionParser(option_list=option_list))
