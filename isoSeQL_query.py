e523576223f38d204f547c1b7fa628ea89846f7a
christine.liu <christine.liu@berkeley.edu>
2022-10-06 18:19:50 -0700

    FSM label read count

--- isoSeQL_query.py
+++ isoSeQL_query.py
@@ -146,7 +146,9 @@ def gene_FSM(db, exp, outPrefix, genes, cutoff):
 		for x in df_gene_plot.tx.unique():
 			plot_df=df_gene_plot[df_gene_plot.tx==x]
 			fig.add_trace(go.Bar(x=plot_df.exp, y=plot_df.Proportion, name=x))
+		total_labels=[{"x":x, "y":1.05, "text":total, "showarrow":False} for x, total in zip(plot_df.exp, plot_df.gene_total)]
 		fig.update_xaxes(categoryorder='array', categoryarray=[int(i) for i in exp_list])
+		total_labels= [{"x":x, "y": 1.05, "text": str()}]
 		fileName=outPrefix+"_FSM_"+g+".pdf"
 		fig.write_image(fileName)
 		print("FSM read proportions plot saved: " + fileName)
