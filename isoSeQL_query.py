dcc26e6714f4fe8fd3765417024c2ee13cf65b2e
christine.liu <christine.liu@berkeley.edu>
2022-10-11 11:45:48 -0700

    plot sizing

--- isoSeQL_query.py
+++ isoSeQL_query.py
@@ -149,7 +149,7 @@ def gene_FSM(db, exp, outPrefix, genes, cutoff):
 			showlegend=True,
 			height=450,
 			width=40*len(exp_list)+140, 
-			legend=dict(font=dict(size=6))
+			legend=dict(font=dict(size=6), itemsizing='constant')
 		)
 		for x in df_gene_plot.tx.unique():
 			plot_df=df_gene_plot[df_gene_plot.tx==x]
