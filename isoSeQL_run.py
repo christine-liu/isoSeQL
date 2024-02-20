d9fc77798eb65da322fb7900e47ffc7c38002ae0
christine.liu <christine.liu@berkeley.edu>
2024-02-20 10:17:42 -0800

    fix tabs again

--- isoSeQL_run.py
+++ isoSeQL_run.py
@@ -16,10 +16,10 @@ def main():
 	parser.add_argument("--expConfig", help="path to exp config file")
 	parser.add_argument("--db", help="path to database")
 	parser.add_argument("--sc", default=None, help='path to annotated csv')
-    parser.add_argument("--gff", default=None, help='path to gff from pigeon')
+	parser.add_argument("--gff", default=None, help='path to gff from pigeon')
 	pkgDir=os.path.dirname(os.path.realpath(__file__))
 	GTF2GENEPRED=os.path.join(pkgDir, "gtfToGenePred")
-    GFFREAD="gffread"
+	GFFREAD="gffread"
 
 	args=parser.parse_args()
 	#check for file existence
