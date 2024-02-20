3e9044bad965af58d2d0445f09cf0f0cf64a9b10
christine.liu <christine.liu@berkeley.edu>
2024-02-20 10:15:33 -0800

    fix tabs

--- isoSeQL_run.py
+++ isoSeQL_run.py
@@ -16,30 +16,30 @@ def main():
 	parser.add_argument("--expConfig", help="path to exp config file")
 	parser.add_argument("--db", help="path to database")
 	parser.add_argument("--sc", default=None, help='path to annotated csv')
-        parser.add_argument("--gff", default=None, help='path to gff from pigeon'))
+    parser.add_argument("--gff", default=None, help='path to gff from pigeon'))
 	pkgDir=os.path.dirname(os.path.realpath(__file__))
 	GTF2GENEPRED=os.path.join(pkgDir, "gtfToGenePred")
-        GFFREAD="gffread"
+    GFFREAD="gffread"
 
-        args=parser.parse_args()
+	args=parser.parse_args()
 	#check for file existence
 	errorMsg=""
 	if not os.path.isfile(args.classif):
 		errorMsg+="\nclassif file: " + args.classif + " not found.\n"
-        if args.genePred and args.gff:
-                errorMsg+"\ngenePred and gff provided. Please only choose one\n"
-        if not args.genePred:
-            if args.gff:
-                if not os.path.isfile(args.gff):
-                    errorMsg+="\n gff file: " + args.gff + " not found.\n"
-                else:
-                    gtfFile=os.path.dirname(os.path.realpath(args.db)) + args.gff + ".gtf"
-                    subprocess.call([GFFREAD, args.gff, '-T', '-o', gtfFile])
-                    genePredFile_fromGff=os.path.dirname(os.path.realpath(args.db)) + args.gff + ".genePred"
-                    subprocess.call([GTF2GENEPRED, gtfFile, genePred, "-genePredExt", "-allErrors", "-ignoreGroupsWithoutExons"])
-        if args.genePred:
-            if not os.path.isfile(args.genePred):
-                errorMsg+="\n genePred file: " + args.genePred + " not found.\n"
+	if args.genePred and args.gff:
+		errorMsg+"\ngenePred and gff provided. Please only choose one\n"
+	if not args.genePred:
+		if args.gff:
+			if not os.path.isfile(args.gff):
+				errorMsg+="\n gff file: " + args.gff + " not found.\n"
+			else:
+				gtfFile=os.path.dirname(os.path.realpath(args.db)) + args.gff + ".gtf"
+				subprocess.call([GFFREAD, args.gff, '-T', '-o', gtfFile])
+				genePredFile_fromGff=os.path.dirname(os.path.realpath(args.db)) + args.gff + ".genePred"
+				subprocess.call([GTF2GENEPRED, gtfFile, genePred, "-genePredExt", "-allErrors", "-ignoreGroupsWithoutExons"])
+	if args.genePred:
+		if not os.path.isfile(args.genePred):
+			errorMsg+="\n genePred file: " + args.genePred + " not found.\n"
 	if not os.path.isfile(args.sampleConfig):
 		errorMsg+="\nsampleConfig file: " + args.sampleConfig + " not found.\n"
 	if not os.path.isfile(args.expConfig):
@@ -54,10 +54,10 @@ def main():
 		print("\nAll files located.\n")		
 
 	classifInfo=fileParse.parse_classification(args.classif)
-        if args.gff:
-            genePredInfo=fileParse.parse_genePred(genePredFile_fromGff)
-        elif args.genePred:
-	    genePredInfo=fileParse.parse_genePred(args.genePred)
+	if args.gff:
+		genePredInfo=fileParse.parse_genePred(genePredFile_fromGff)
+	elif args.genePred:
+		genePredInfo=fileParse.parse_genePred(args.genePred)
 	if args.sc:
 		scInfo,UMIs=fileParse.parse_singleCell(args.sc)
 	else:
